787c18532057358d42e9b2912bc860683b100f99
AlexWolski <alex.theinfinite@gmail.com>
2019-07-28 23:53:38 -0700

    Loads RCAssets AssetBundle

---
+++ AOTTG Map Editor/Assets/Scripts/AssetManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class AssetManager : MonoBehaviour
+{
+    private AssetBundle RCAssets;
+
+    void Start()
+    {
+        base.StartCoroutine(LoadRCAssets());
+    }
+
+    //Load the RC mod assets from RCAssets.unity3d
+    private IEnumerator LoadRCAssets()
+    {
+        //Reference to the file. Compatible with windows and Mac.
+        string url = "File:///" + Application.dataPath + "/RCAssets.unity3d";
+
+        //Wait until caching is available
+        while (!Caching.ready)
+            yield return null;
+
+        //Load the file
+        using (WWW iteratorVariable2 = WWW.LoadFromCacheOrDownload(url, 1))
+        {
+            RCAssets = iteratorVariable2.assetBundle;
+        }
+    }
+
+    //Instantiate a GameObject wtih the given name from the RCAssets AssetBundle
+    public GameObject instantiateRCAsset(string objectName, Vector3 scale, Vector3 position, Quaternion angle)
+    {
+        //Instantiate the object with the given position and angle
+        GameObject newObject = (GameObject) Instantiate((GameObject) RCAssets.Load(objectName), position, angle);
+
+        //Transform the object
+        Vector3 currentScale = newObject.transform.localScale;
+        newObject.transform.localScale = new Vector3(currentScale.x * scale.x, currentScale.y * scale.y, currentScale.z * scale.z);
+
+        return newObject;
+    }
+}
---
+++ AOTTG Map Editor/Assets/Scripts/Test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+    public AssetManager assetManager;
+
+	void Start () {
+        StartCoroutine(TestInstantiate());
+    }
+
+    IEnumerator TestInstantiate()
+    {
+        yield return new WaitForSeconds(1);
+        assetManager.instantiateRCAsset("cuboid", new Vector3(1, 1, 1), new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
+    }
+	
+	void Update () {
+	
+	}
+}
