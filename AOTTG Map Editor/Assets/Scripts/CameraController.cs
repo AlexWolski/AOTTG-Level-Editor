30c7a494dcb245e8b8699bd70c6e248435e89ea6
AlexWolski <alex.theinfinite@gmail.com>
2019-07-30 02:12:59 -0700

    Added missing scripts

---
+++ AOTTG Map Editor/Assets/Scripts/CameraController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //A reference to the main object so the EditorManager is accessable
+    public GameObject mainObject;
+    //A reference to the editorManager
+    private EditorManager editorManager;
+    //The three speeds the camera can move at
+    public float fastSpeed;
+    public float normalSpeed;
+    public float slowSpeed;
+    //The speed the camera rotates at
+    public float rotateSpeed;
+
+    void Start()
+    {
+        //Get the EditorManager from the main object
+        editorManager = mainObject.GetComponent<EditorManager>();
+    }
+
+    void Update()
+    {
+        //If the editor is fly mode, translate and rotate the camera
+        if(editorManager.editMode == "fly")
+        {
+            translateCamera();
+            rotateCamera();
+        }
+    }
+
+    private void translateCamera()
+    {
+        //The speed the camera should move at
+        float currentSpeed = normalSpeed;
+
+        //Set the speed based on if shift, control, or neither is pressed
+        if (Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl))
+            currentSpeed = slowSpeed;
+        else if (Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift))
+            currentSpeed = fastSpeed;
+
+        //Get the amount to translate on the x and z axis
+        float xDisplacement = Input.GetAxisRaw("Horizontal") * currentSpeed * Time.deltaTime;
+        float zDisplacement = Input.GetAxisRaw("Vertical") * currentSpeed * Time.deltaTime;
+
+        //Get the amount to translate on the y axis
+        float yDisplacement = 0;
+
+        //If only the left mouse button is pressed, move the camera down
+        if (Input.GetButton("Fire1") && !Input.GetButton("Fire2"))
+            yDisplacement = -currentSpeed * Time.deltaTime;
+        //If only the right mouse button is pressed, move the camera up
+        else if (Input.GetButton("Fire2") && !Input.GetButton("Fire1"))
+            yDisplacement = currentSpeed * Time.deltaTime;
+
+        //Translate the camera on the x and z axes in self space
+        transform.Translate(xDisplacement, 0, zDisplacement, Space.Self);
+        //Translate the camera on the y axis in world space
+        transform.Translate(0, yDisplacement, 0, Space.World);
+    }
+
+    private void rotateCamera()
+    {
+        //Find how much the camera should be rotated on the x and y axes, then add the current rotations to them
+        float xRotation = (Input.GetAxis("Mouse Y") * -rotateSpeed * Time.deltaTime) + transform.rotation.eulerAngles.x;
+        float yRotation = (Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime) + transform.rotation.eulerAngles.y;
+
+        //Restrict the camera angle so it doesn't flip
+        if (xRotation > 90 && xRotation < 180)
+            xRotation = 90;
+        if (xRotation < 270 && xRotation > 180)
+            xRotation = 270;
+
+        //Set the new rotation of the camera
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+    }
+}
---
+++ AOTTG Map Editor/Assets/Scripts/EditorManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorManager : MonoBehaviour
+{
+    //Determines if the user is in fly more or edit mode. Default mode is fly
+    public string editMode;
+
+    void Update()
+    {
+        //If the x key is pressed, toggle between edit and fly mode
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if(editMode == "fly")
+            {
+                editMode = "edit";
+                Screen.lockCursor = false;
+            }
+            else if(editMode == "edit")
+            {
+                editMode = "fly";
+                Screen.lockCursor = true;
+            }
+        }
+    }
+}
