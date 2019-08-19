a6b07d4915e33ad2584c4d1075a88278e4f3f155
Alex Wolski <awolski@mail.sfsu.edu>
2019-08-19 15:53:52 -0700

    Fixed window resizing and added resize cursor

--- AOTTG Map Editor/Assets/External Assets/FlexibleUISystems/FlexibleDraggableObject/Scripts/FlexibleResizeHandler.cs
+++ AOTTG Map Editor/Assets/External Assets/FlexibleUISystems/FlexibleDraggableObject/Scripts/FlexibleResizeHandler.cs
@@ -22,20 +22,58 @@ public class FlexibleResizeHandler : MonoBehaviour
     public RectTransform Target;
     public Vector2 MinimumDimmensions = new Vector2(50, 50);
     public Vector2 MaximumDimmensions = new Vector2(800, 800);
-    
+
+    private static Canvas parentCanvas;
     private EventTrigger _eventTrigger;
+    private Vector2 dragStartPoint;
+    private Vector2 rectStartSize;
+    private static Texture2D ewResizeImage;
+    private static Vector2 cursorHotSpot;
+    private static bool resizeBoxPressed;
     
 	void Start ()
 	{
 	    _eventTrigger = GetComponent<EventTrigger>();
+        _eventTrigger.AddEventTrigger(OnMouseEnter, EventTriggerType.PointerEnter);
+        _eventTrigger.AddEventTrigger(OnMouseExit, EventTriggerType.PointerExit);
+        _eventTrigger.AddEventTrigger(OnMouseDown, EventTriggerType.PointerDown);
+        _eventTrigger.AddEventTrigger(OnMouseUp, EventTriggerType.PointerUp);
         _eventTrigger.AddEventTrigger(OnDrag, EventTriggerType.Drag);
-	}
+        _eventTrigger.AddEventTrigger(OnEndDrag, EventTriggerType.EndDrag);
+        dragStartPoint = new Vector2(0, 0);
+        rectStartSize = new Vector2(0, 0);
+        parentCanvas = GetComponentInParent<Canvas>();
+        ewResizeImage = (Texture2D)Resources.Load("GUI/ew-resize", typeof(Texture2D));
+        cursorHotSpot = new Vector2(ewResizeImage.width/2, ewResizeImage.height/2);
+    }
+
+    void OnMouseEnter(BaseEventData data)
+    {
+        Cursor.SetCursor(ewResizeImage, cursorHotSpot, CursorMode.ForceSoftware);
+    }
+
+    void OnMouseExit(BaseEventData data)
+    {
+        if(!resizeBoxPressed)
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    void OnMouseDown(BaseEventData data)
+    {
+        PointerEventData ped = (PointerEventData)data;
+        dragStartPoint.Set(ped.position.x, ped.position.y);
+        rectStartSize.Set(Target.rect.width, Target.rect.height);
+        resizeBoxPressed = true;
+    }
+
+    void OnMouseUp(BaseEventData data)
+    {
+        resizeBoxPressed = false;
+    }
 
     void OnDrag(BaseEventData data)
     {
         PointerEventData ped = (PointerEventData) data;
-        //Target.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Target.rect.width + ped.delta.x);
-        //Target.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Target.rect.height + ped.delta.y);
         RectTransform.Edge? horizontalEdge = null;
         RectTransform.Edge? verticalEdge = null;
 
@@ -76,10 +114,10 @@ public class FlexibleResizeHandler : MonoBehaviour
         {
             if (horizontalEdge == RectTransform.Edge.Right)
                 Target.SetInsetAndSizeFromParentEdge((RectTransform.Edge)horizontalEdge, 0,
-                    Mathf.Clamp(Target.rect.width - ped.delta.x, MinimumDimmensions.x, MaximumDimmensions.x));
+                    Mathf.Clamp(rectStartSize.x - (ped.position.x - dragStartPoint.x)/parentCanvas.scaleFactor, MinimumDimmensions.x, MaximumDimmensions.x));
             else
-                Target.SetInsetAndSizeFromParentEdge((RectTransform.Edge)horizontalEdge, 0, 
-                    Mathf.Clamp(Target.rect.width + ped.delta.x, MinimumDimmensions.x, MaximumDimmensions.x));
+                Target.SetInsetAndSizeFromParentEdge((RectTransform.Edge)horizontalEdge, 0,
+                    Mathf.Clamp(rectStartSize.x + (ped.position.x - dragStartPoint.x)/ parentCanvas.scaleFactor, MinimumDimmensions.x, MaximumDimmensions.x));
         }
         if (verticalEdge != null)
         {
@@ -92,5 +130,10 @@ public class FlexibleResizeHandler : MonoBehaviour
                     Target.position.y - Target.pivot.y * Target.rect.height, 
                     Mathf.Clamp(Target.rect.height + ped.delta.y, MinimumDimmensions.y, MaximumDimmensions.y));
         }
+    }
+
+    void OnEndDrag(BaseEventData data)
+    {
+        Cursor.SetCursor(null, cursorHotSpot, CursorMode.Auto);
     }
 }
