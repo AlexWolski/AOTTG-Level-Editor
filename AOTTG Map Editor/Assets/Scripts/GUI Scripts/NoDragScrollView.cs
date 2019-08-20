21040dacf1768fcd69c7a3bba5cf49ab12f973ba
Alex Wolski <awolski@mail.sfsu.edu>
2019-08-19 23:07:34 -0700

    Disabled mouse dragging in scroll view

---
+++ AOTTG Map Editor/Assets/Scripts/GUI Scripts/NoDragScrollView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class NoDragScrollView : ScrollRect
+{
+    public override void OnBeginDrag(PointerEventData eventData) {
+        eventData.pointerDrag = null;
+    }
+    public override void OnDrag(PointerEventData eventData) {
+        eventData.pointerDrag = null;
+    }
+    public override void OnEndDrag(PointerEventData eventData) {
+        eventData.pointerDrag = null;
+    }
+}
