9c5c07584a9587815068eb0ab562ebc4e21879a2
AlexWolski <alex.theinfinite@gmail.com>
2020-03-18 20:35:59 -0700

    Reimported fbx files and applied correct texture to all

--- AOTTG Map Editor/Assets/Scripts/Level Editor/AssetManager.cs
+++ AOTTG Map Editor/Assets/Scripts/Level Editor/AssetManager.cs
@@ -5,50 +5,9 @@ using System.Collections.Generic;
 
 public static class AssetManager
 {
-    //The unity3d file that contains the assets from RC mod
-    private static AssetBundle RCAssets;
     //The locations of the prefabs and materials in the resources folder
     private readonly static string prefabFolder = "Prefabs/RC Assets/";
     private readonly static string materialFolder = "Materials/RC Assets/";
-
-    //Load the RC mod assets from RCAssets.unity3d
-    [System.Obsolete]
-    public static IEnumerator LoadRCAssets()
-    {
-        //Reference to the file. Compatible with windows and Mac.
-        string url = "File:///" + Application.dataPath + "/RCAssets.unity3d";
-
-        //Wait until caching is available
-        while (!Caching.ready)
-            yield return null;
-
-        //Load the file
-        using (WWW iteratorVariable2 = WWW.LoadFromCacheOrDownload(url, 1))
-        {
-            RCAssets = iteratorVariable2.assetBundle;
-
-            if (!RCAssets)
-            {
-                Debug.Log("RC Assets Didn't Load Successfully");
-                Debug.Log("RC Assets File Location: " + url);
-            }
-        }
-    }
-
-    public static Texture[] loadAllTextures()
-    {
-        return RCAssets.LoadAllAssets<Texture>();
-    }
-
-    public static Material[] loadAllMaterials()
-    {
-        return RCAssets.LoadAllAssets<Material>();
-    }
-
-    public static GameObject[] loadAllGameObjects()
-    {
-        return RCAssets.LoadAllAssets<GameObject>();
-    }
 
     //Instantiate the GameObject wtih the given name
     public static GameObject instantiateRcObject(string objectName)
