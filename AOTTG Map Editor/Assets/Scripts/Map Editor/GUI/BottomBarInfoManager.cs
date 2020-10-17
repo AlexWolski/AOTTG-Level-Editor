f0f66834aaad2ec04b5ebf70aeae274198dd95a3
AlexWolski <alex.theinfinite@gmail.com>
2020-10-17 16:14:10 -0700

    Added a BottomBarInfoManager class

---
+++ AOTTG Map Editor/Assets/Scripts/Map Editor/GUI/BottomBarInfoManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MapEditor
+{
+    public class BottomBarInfoManager : MonoBehaviour
+    {
+        //The different groups that are displayed based on the current mode
+        [SerializeField] private GameObject flyEditMode;
+        [SerializeField] private GameObject commandMode;
+        [SerializeField] private GameObject dragMode;
+
+        //The game object containing the text for the current mode
+        [SerializeField] private GameObject currentMode;
+        //The text object for the current mode
+        private Text currentModeText;
+
+        private void Start()
+        {
+            //Find and store the needed component references
+            currentModeText = currentMode.GetComponent<Text>();
+
+            //Listen for when the editor mode is changed
+            EditorManager.Instance.OnChangeMode += OnModeChange;
+        }
+
+        private void OnModeChange(EditorMode prevMode, EditorMode newMode)
+        {
+            //Set the mode text to display the current mode
+            currentModeText.text = newMode.ToString();
+        }
+    }
+}
