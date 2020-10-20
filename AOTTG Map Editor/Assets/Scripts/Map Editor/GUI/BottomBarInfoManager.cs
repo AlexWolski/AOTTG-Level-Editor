5f22f2d8437b5db1aab8fb8993ad7649031c1263
AlexWolski <alex.theinfinite@gmail.com>
2020-10-19 20:48:52 -0700

    Added movement key info to the bottom bar

--- AOTTG Map Editor/Assets/Scripts/Map Editor/GUI/BottomBarInfoManager.cs
+++ AOTTG Map Editor/Assets/Scripts/Map Editor/GUI/BottomBarInfoManager.cs
@@ -5,8 +5,11 @@ namespace MapEditor
 {
     public class BottomBarInfoManager : MonoBehaviour
     {
-        //The different groups that are displayed based on the current mode
+        //The info displayed when in fly or edit mode
         [SerializeField] private GameObject flyEditMode;
+
+        //Groups that show info for the current state
+        [SerializeField] private GameObject movementKeys;
 
         //The game object and text component displaying the current mode
         [SerializeField] private GameObject currentMode;
@@ -36,9 +39,12 @@ namespace MapEditor
             //If the new mode is UI mode, hide the current mode info.
             if (newMode == EditorMode.UI)
                 flyEditMode.SetActive(false);
-            //If the previous mode was UI mode, show the current mode info again.
+            //If new mode is Fly or Edit, show the current mode info again.
             else if (prevMode == EditorMode.UI)
                 flyEditMode.SetActive(true);
+
+            //Toggle the movement keys based on if the current mode is fly mode
+            movementKeys.SetActive(newMode == EditorMode.Fly);
 
             //Set the mode text to display the current mode
             currentModeText.text = newMode.ToString();
